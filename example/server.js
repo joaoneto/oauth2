0c5ef266468f4645848f29513866ab34160201b9
João Neto <joaopintoneto@gmail.com>
2013-03-08 17:41:47 -0300

    Added access token verification

--- example/server.js
+++ example/server.js
@@ -12,7 +12,13 @@ app.configure(function () {
   app.use(express.cookieParser());
 });
 
+// Oauth2 endpoint
 app.post('/', oauth2.grantAccessToken());
+
+// Restricted resource
+app.get('/resource', oauth2.verifyAccessToken(), function (req, res) {
+  res.json({test: 'restrict_resource'});
+});
 // console.log(oauth2.parseHeaderParams());
 
 app.listen(3000, function () {
