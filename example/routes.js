e146934d354b1a6c7a745aa39e725ca4b120868b
João Neto <joaopintoneto@gmail.com>
2013-03-11 17:17:06 -0300

    Separate routes on example

---
+++ example/routes.js
@@ -0,0 +1,56 @@
+module.exports = function (oauth2) {
+  var Client = oauth2.Client;
+
+  var _listClients = function (req, res) {
+    var secret = oauth2.genClientSecret();
+
+    Client.find({}).sort({'_id': -1}).execFind(function (err, docs) {
+      if (err)
+        return null;
+
+      var result = ['<html><body>'];
+      result.push('<form action="", method="post">Client secret: <input name="secret" readonly="readyonly" value="');
+      result.push(secret);
+      result.push('"> Redirect uri: <input name="redirect_uri"><input type="submit"></form>');
+      result.push('<h2>List</h2><table><tr><td>client_id</td><td>client_secret</td><td>request_uri</td></tr>');
+
+      for (var x = 0, len = docs.length; x < len; x++)
+        result.push(['<tr><td>', docs[x]._id, '</td><td>', docs[x].secret, '</td><td>', docs[x].redirect_uri, '</td></tr>'].join(''));
+
+      result.push('</table>');
+      result.push('</body></html>')
+
+      res.send(result.join(''));
+    });
+  };
+
+  var _createClient = function (req, res) {
+    if (!req.body || !req.body.redirect_uri || !req.body.secret)
+      res.send('Client secret or redirect uri invalid, or not present, please try again!').end();
+
+    var new_client = new Client(req.body);
+    new_client.save(function (err, client) {
+
+      if (err)
+        return req.send('Error saving new client, please try again!').end();
+
+      var result = ['<h2>New client created!</h2><a href="/clients">« back to list</a>'];
+
+      result.push('<table><tr><td>client_id</td><td>client_secret</td><td>redirect_uri</td></tr>');
+      result.push('<tr><td>');
+      result.push(client._id);
+      result.push('</td><td>');
+      result.push(client.secret);
+      result.push('</td><td>');
+      result.push(client.redirect_uri);
+      result.push('</td></tr></table>');
+
+      res.send(result.join('')).end();
+    });
+  };
+
+  return {
+    listClients: _listClients,
+    createClient: _createClient
+  };
+}
--- example/server.js
+++ example/server.js
@@ -1,49 +1,24 @@
 var express = require('express'),
   mongoose = require('mongoose');
 
+// Init oauth2
 var oauth2 = require('../lib/oauth2');
-
 oauth2.driver('mongoose', { db: 'mongodb://localhost/teste' });
 
-// OAuth2 mongoose models
-var Client = oauth2.Client;
+var routes = require('./routes')(oauth2);
 
+// Init app
 var app = module.exports = express();
-
 app.configure(function () {
   app.use(express.bodyParser());
   app.use(express.cookieParser());
 });
 
-// New client form
-app.get('/clients', function (req, res) {
-  var secret = oauth2.genClientSecret();
-  res.send('<html><body><form action="", method="post">Client secret: <input name="secret" readonly="readyonly" value="' + secret + '"> Redirect uri: <input name="redirect_uri"><input type="submit"></form></body></html>');
-});
+// List and create client form
+app.get('/clients', utils.listClients);
 
-// Add new client and list clients
-app.post('/clients', function (req, res) {
-  if (!req.body || !req.body.redirect_uri || !req.body.secret)
-    res.send('Client secret or redirect uri invalid, or not present, please try again!').end();
-
-  var new_client = new Client(req.body);
-  new_client.save(function (err, client) {
-    if (err)
-      return req.send('Error saving new client, please try again!').end();
-
-    Client.find({}).sort({'_id': -1}).execFind(function (err, docs) {
-      var result = ['<table>\n<tr><td>client_id</td><td>client_secret</td><td>request_uri</td></tr>\n'];
-
-      for (var x = 0, len = docs.length; x < len; x++)
-        result.push(['<tr><td>', docs[x]._id, '</td><td>', docs[x].secret, '</td><td>', docs[x].redirect_uri, '</td></tr>\n'].join(''));
-
-      result.push(['</table>\n']);
-
-      res.send(result.join('')).end();
-    });
-
-  });
-});
+// Add new client
+app.post('/clients', utils.createClient);
 
 // Oauth2 authorization
 app.post('/authorization', oauth2.handleAuthorization());
